bb9d3116d3ab5ce872a252be7088020cf1f03115
jiewu-stanford <bayernscience@hotmail.com>
2019-02-15 14:56:00 +0800

    R script for cleaning Samsung mobile phone user data

---
+++ Codebook.md
@@ -0,0 +1,14 @@
+# Getting and Cleaning Data Project
+
+### Description
+The goal is to prepare tidy data that can be used for later analysis. The data are collected from the accelerometers from the Samsung Galaxy S smartphone. A full description is available at the site where the data was obtained:
+
+[http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)
+
+
+### Data Set Information
+The experiments have been carried out with a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone (Samsung Galaxy S II) on the waist. Using its embedded accelerometer and gyroscope, we captured 3-axial linear acceleration and 3-axial angular velocity at a constant rate of 50Hz. The experiments have been video-recorded to label the data manually. The obtained dataset has been randomly partitioned into two sets, where 70% of the volunteers was selected for generating the training data and 30% the test data.
+
+
+### Variables
+The subjects are coded from 1 to 30. The activities are coded from 1 to 6 and are then labeled with names during the processing. The measurement (the feature variable) has three attributes (1) what kind of measurement e.g. body acceleration or jerk (2) what type of statistical data i.e. mean or standard deviation (3) what is the axis the measurement was made i.e. X or Y or Z.
---
+++ run_analysis.R
@@ -0,0 +1,87 @@
+## load necessary packages
+library(data.table)     ## load library for data.table(), fread()
+library(dplyr)          ## load library for select()
+library(reshape2)       ## load library for melt()
+library(tidyr)          ## load library for separate()
+## download data file
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(url, "dataFiles.zip")
+## unzip and list files
+unzip(zipfile = "dataFiles.zip")
+list.files('./UCI HAR Dataset',recursive=TRUE)
+
+
+####################################################################
+# 1. merge training and test set
+####################################################################
+## Load train datasets
+subjectTrain <- fread("./UCI HAR Dataset/train/subject_train.txt")
+subjectTest <- fread("./UCI HAR Dataset/test/subject_test.txt")
+activityTrain <- fread("./UCI HAR Dataset/train/Y_train.txt")
+activityTest <- fread("./UCI HAR Dataset/test/Y_test.txt")
+XTrain <- fread("./UCI HAR Dataset/train/X_train.txt")
+XTest <- fread("./UCI HAR Dataset/test/X_test.txt")
+## combine test and train subject
+subjectDT <- rbind(subjectTrain, subjectTest)
+setnames(subjectDT, "V1", "subjectCode")
+## combine test and train activity
+activityDT <- rbind(activityTrain, activityTest)
+setnames(activityDT, "V1", "activityCode")
+## combine X feature codes
+DT <- rbind(XTrain,XTest)
+## merge columns
+DT <- cbind(subjectDT, DT)
+DT <- cbind(DT,activityDT)
+
+
+####################################################################
+# 2. extract only the mean and standard deviation for each mesure
+####################################################################
+## get the feature code table
+featureDT <- fread("./UCI HAR Dataset/features.txt",
+                                    col.names=c("indx", "feature"))
+## extract the mean and standard deviation measures
+featureDT <- featureDT[grepl('mean\\(\\)|std\\(\\)',feature)]
+## add a column of feature code
+featureDT$featureCode <- featureDT[,paste0('V',indx)]
+DTmeanstd <- select(DT,c('subjectCode',featureDT$featureCode,'activityCode'))
+
+
+####################################################################
+# 3. uses descriptive activity names to name the activities
+####################################################################
+activitylabel <- fread("./UCI HAR Dataset/activity_labels.txt")
+setnames(activitylabel, names(activitylabel), c("indx", "activity"))
+
+
+####################################################################
+# 4. label data set using descriptive activity names
+####################################################################
+## label activity
+DTmeanstdlabeled <- merge(DTmeanstd,activitylabel,by.x='activityCode',
+                                    by.y='indx',all.x=TRUE)
+setkey(DTmeanstdlabeled,subjectCode,activityCode,activity)
+## melt to add a column of feature code
+DTtbl <- melt(DTmeanstdlabeled,key(DTmeanstdlabeled),
+                                    variable.name='featureCode')
+## label feature
+DTl <- merge(DTtbl,featureDT,by='featureCode',all.x=TRUE)
+
+
+####################################################################
+# 5. find the average of each variable for each activity and each subject
+####################################################################
+## remove brackets
+DTl$feature <- gsub("\\(\\)","",DTl$feature)
+## check the following command for separate result
+## separate(featureDT,feature,c('measure','type','axis'),sep='-')
+DTtidy <- separate(DTl,feature,c('measure','type','axis'),sep='-')
+DTtidy <- DTtidy[,c('subjectCode','activity','measure','type','axis','value')]
+## check result
+## head(DT[,c('subjectCode','activityCode','V1')])
+## head(DTtidy[DTtidy$activity=='STANDING',])
+DTstats <- DTtidy[, list(count=.N, average=mean(value)), by=c('subjectCode',
+                                    'activity','measure','type','axis')]
+head(DTstats)
+## output data table
+write.table(DTstats,file='./tidydata.txt',row.name=FALSE)
